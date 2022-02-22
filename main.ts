89023630dd6e551b29956d0fe5f00be01f89172c
MateoGeremia <100204333+MateoGeremia@users.noreply.github.com>
2022-02-22 14:16:58 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,3 +9,25 @@ input.onButtonPressed(Button.A, function () {
         . . . # #
         `)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("133333.33333333334 foie 3")
+    basic.showString("" + (133333.33333333334 * 3))
+    basic.showLeds(`
+        # # . # #
+        # # # # #
+        . # # # .
+        # # # # #
+        # # . # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("400000 div 3")
+    basic.showString("" + (400000 / 3))
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
